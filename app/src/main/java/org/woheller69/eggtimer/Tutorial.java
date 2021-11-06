d107541977059badbb4c36c9c8b02828d4dbe076
Clone Conflict <cloneconf@gmail.com>
2021-11-06 11:31:58 +0100

    fixing github button.

--- app/src/main/java/org/woheller69/eggtimer/Tutorial.java
+++ app/src/main/java/org/woheller69/eggtimer/Tutorial.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class Tutorial extends AppCompatActivity {
 
