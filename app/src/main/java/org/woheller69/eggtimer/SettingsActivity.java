76e3cc93db967b74d09f5ac0fe1ee2e63667834b
woheller69 <wolfgang.heller@web.de>
2022-11-05 14:52:21 +0100

    add actionbar to Settings and Tutorial add onSharedPreferenceChangeListener to Settings

--- app/src/main/java/org/woheller69/eggtimer/SettingsActivity.java
+++ app/src/main/java/org/woheller69/eggtimer/SettingsActivity.java
@@ -1,15 +1,20 @@
 package org.woheller69.eggtimer;
 
+import static java.lang.Boolean.TRUE;
+
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,12 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext()).registerOnSharedPreferenceChangeListener(this);
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -54,4 +65,17 @@ public class SettingsActivity extends AppCompatActivity {
             });
         }
     }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if (s.equals("useGPS")) {
+            if (sharedPreferences.getBoolean(s, true) == TRUE) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,}, 1);
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/org/woheller69/eggtimer/Tutorial.java
+++ app/src/main/java/org/woheller69/eggtimer/Tutorial.java
@@ -1,5 +1,6 @@
 package org.woheller69.eggtimer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,7 +14,12 @@ public class Tutorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
+
     public void openGithub(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/woheller69/eggtimer")));
     }
